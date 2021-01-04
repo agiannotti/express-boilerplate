d1fb13f737eb6083010a10811acea9407d7b1b50
tonyg <aj.gianno@gmail.com>
2021-01-04 15:31:06 -0600

    Error handler

--- src/app.js
+++ src/app.js
@@ -5,15 +5,35 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 
 const app = express();
-
+//PIPELINE begins
+// Standard middleware
 app.use(cors());
 app.use(helmet());
 
 const morganOption = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 
+//Routes
 app.get('/', (req, res) => {
+  JSON.parse('{key : "value"}');
+
   res.json({ message: 'Hello, world!' });
 });
 
+// Error handlers
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { message: 'Internal server error' };
+  } else {
+    console.error(error);
+    response = { error, message: error.message };
+  }
+  res.status(500).json(response);
+});
+
+//PIPELINE ends
+
 module.exports = app;
+
+function middlewareFunc(req, res, next) {}
